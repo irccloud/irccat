c60c59bfac3f722dc38abb323354a314f49f26bc
Russ Garrett <russ@garrett.co.uk>
2017-01-25 09:36:45 +0000

    Fix formatting in github releases handler

--- httplistener/github-releases.go
+++ httplistener/github-releases.go
@@ -14,15 +14,15 @@ type githubRepository struct {
 }
 
 type githubRelease struct {
-	Html_url    string
-	Tag_name    string
-	Name        string
+	Html_url string
+	Tag_name string
+	Name     string
 }
 
 type githubPayload struct {
-	Action      string
-	Release     githubRelease
-	Repository  githubRepository
+	Action     string
+	Release    githubRelease
+	Repository githubRepository
 }
 
 func (hl *HTTPListener) githubReleasesHandler(w http.ResponseWriter, request *http.Request) {
@@ -35,7 +35,7 @@ func (hl *HTTPListener) githubReleasesHandler(w http.ResponseWriter, request *ht
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(request.Body)
 	json.Unmarshal(buf.Bytes(), &payload)
-	
+
 	if payload.Action == "published" {
 		name := payload.Repository.Description
 		if name == "" {
@@ -45,7 +45,7 @@ func (hl *HTTPListener) githubReleasesHandler(w http.ResponseWriter, request *ht
 		if release == "" {
 			release = payload.Release.Tag_name
 		}
-		
+
 		msg := fmt.Sprintf("[\x02Release\x0f] \x02%s\x0f version \x02%s\x0f has been published: %s", name, release, payload.Release.Html_url)
 
 		channelKey := fmt.Sprintf("http.listeners.github-releases.%s", payload.Repository.Name)
